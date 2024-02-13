946805fdacec8bf4a143900b41ea91336863e727
Vijay kumar <96686531+vijaykr85@users.noreply.github.com>
2024-02-13 15:22:44 +0530

    Add project files.

---
+++ JWT_Token_Example/Controllers/EmployeeController.cs
@@ -0,0 +1,34 @@
+﻿using JWT_Token_Example.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWT_Token_Example.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        [Authorize]
+        [HttpGet]
+        [Route("GetData")]
+        public string GetData()
+        {
+            return "Authenticated with JWT";
+        }
+
+        [HttpGet]
+        [Route("Details")]
+        public string Details()
+        {
+            return "Authenticated with JWT";
+        }
+
+        [Authorize]
+        [HttpPost]
+        public string AddUser(Users user)
+        {
+            return "User added with username" + user.UserName;
+        }
+    }
+}
---
+++ JWT_Token_Example/Models/Users.cs
@@ -0,0 +1,8 @@
+﻿namespace JWT_Token_Example.Models
+{
+    public class Users
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
